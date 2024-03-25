1bcdc1d92aafa327097382c4462003eb61b7c5c5
ajsinp <ajaysingh98147@gmail.com>
2024-03-26 00:16:02 +0530

    second

--- src/components/User.js
+++ src/components/User.js
@@ -4,10 +4,6 @@ const User = ({name}) => {
     const [count]=useState(0);
     const [count1]=useState(1);
 
-    useEffect (() =>{
-         //API Calls
-    },[]);
-
     return(
         <div className="user-card">
             <h1>Count = {count}</h1>
--- src/index.js
+++ src/index.js
@@ -16,3 +16,4 @@ import reportWebVitals from './reportWebVitals';
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
