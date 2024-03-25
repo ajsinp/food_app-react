5dbbb26fd3fe3d6ce822ff4f5b9a287b5aaf350f
ajsinp <ajaysingh98147@gmail.com>
2024-03-25 19:04:14 +0530

    Updates

---
+++ static/js/70.a08e9d3d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_react_app=self.webpackChunkmy_react_app||[]).push([[70],{70:(e,a,s)=>{s.r(a),s.d(a,{default:()=>n});var c=s(579);const n=()=>(0,c.jsxs)("h1",{children:[" ","Our grocery online store and we have a lot of child comopnent inside this web page!!"]})}}]);
+//# sourceMappingURL=70.a08e9d3d.chunk.js.map
