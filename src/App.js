7f1acc1e3edd440267f972c0d081c67f86027c82
ajsinp <ajaysingh98147@gmail.com>
2024-03-26 01:14:44 +0530

    changed code

--- src/App.js
+++ src/App.js
@@ -1,78 +1,35 @@
-import React, { lazy,Suspense, useEffect, useState } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter, Outlet} from "react-router-dom";  
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Error from "./components/Error";  
+import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantsMenu";
-import UserContext from "./Utils/UserContext";
 import { Provider } from "react-redux";
 import appStore from "./Utils/appStore";
 import Cart from "./components/Cart";
-//import Grocery from "./components/Grocery";
-
 
 const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
   const [userName, setUserName] = useState();
 
-  useEffect(()=>{
+  useEffect(() => {
     const data = {
-      name:"Ajay Singh" 
+      name: "Ajay Singh"
     }
-    setUserName (data.name); 
-  },[]);
-
+    setUserName(data.name);
+  }, []);
 
   return (
     <Provider store={appStore}>
-      <UserContext.Provider value={{ loggedInUser: userName, setUserName}}>
-        <div className="App">
-          <Header/>  
-          <Outlet/>
-        </div>
-      </UserContext.Provider>
+      <div className="App">
+        <Header />
+        <Outlet />
+      </div>
     </Provider>
-    
-  ); 
+  );
 };
-const appRouter = createBrowserRouter([
-  {
-    path:"/",
-    element:<AppLayout />,
-    children: [
-      {
-        path:"/",
-        element: <Body/>
-      },
-      {
-        path:"/about",
-        element: <About />,
-      },
-      {
-        path:"/contact",
-        element: <Contact />,
-      },
-      {
-        path:"/grocery",
-        element: <Suspense fallback ={<h1>Loading...</h1>}><Grocery /></Suspense>,
-      },
-      {
-        path: "/restaurants/:resId",
-        element: <RestaurantMenu />
-      },
-      {
-        path:"/cart",
-        element: <Cart/>
-      }   
-    ],
-    errorElement:<Error />,
-  },
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter} />);
 
 export default AppLayout;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'; // Import your CSS files or stylesheets
-import App from './App'; // Import the root component of your application
+import ReactDOM from 'react-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AppLayout from './AppLayout'; // Import your AppLayout component
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-  
-// );
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: <Suspense fallback={<h1>Loading...</h1>}><Grocery /></Suspense>,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />
+      },
+      {
+        path: "/cart",
+        element: <Cart />
+      }
+    ],
+    errorElement: <Error />,
+  },
+]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(
+  <RouterProvider router={appRouter} />,
+  document.getElementById('root')
+);
+
 reportWebVitals();
-
